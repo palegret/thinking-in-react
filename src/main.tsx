b48e46f309a7576c783ba21a6c1bca8ba1e8217e
Philip Alegret <philip.alegret@gmail.com>
2024-02-28 02:51:23 -0500

    Initial commit.

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './styles/index.scss'
+
+const element = document.getElementById('root')
+
+const component = (  
+  <React.StrictMode>
+  </React.StrictMode>
+)
+
+ReactDOM.createRoot(element!).render(component)
